dbb5aa74ea93d6d6756fabd1c7d14e5b0b465820
Vinicius Vidal <lvvidal@gmail.com>
2020-08-02 22:23:43 -0300

    Fixing shell script

--- dags/copy/upload_s3.sh
+++ dags/copy/upload_s3.sh
@@ -7,7 +7,7 @@ folder_s3=$2
 folder_local=$3
 file_local=$4
 
-destination_path="s3://${destination_bucket}/${folder_s3}"
+destination_path="s3://${destination_bucket}/${folder_s3}/${file_local}"
 
 echo "destination_path=$destination_path"
 echo "folder_s3=$folder_s3"
