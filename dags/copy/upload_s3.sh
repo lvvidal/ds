daf00f47edf367317601cfff3dba6d5f16922499
Luiz Vinicius Izidorio Vidal <luizv@bariservice.com.br>
2020-08-04 12:20:37 -0300

    Fixing

--- dags/copy/upload_s3.sh
+++ dags/copy/upload_s3.sh
@@ -13,9 +13,10 @@ echo "destination_path=$destination_path"
 echo "folder_s3=$folder_s3"
 echo "folder_local=$folder_local"
 echo "file_local=$file_local"
+echo "destination_path=$destination_path"
 
 cd ${folder_local} || exit 1
-aws s3 cp ${file_local} "${destination_path}"
+aws s3 cp ${file_local} ${destination_path}
 
 
 
